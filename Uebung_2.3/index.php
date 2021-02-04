e2d549801c6d04f7cc035d010540e1b3a08edacc
louis11s <louis_schneider@sluz.ch>
2021-02-04 11:57:44 +0100

    Übung 2 und 3 Commit

--- Uebung_2.3/index.php
+++ Uebung_2.3/index.php
@@ -8,7 +8,6 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
@@ -22,11 +21,14 @@ if ($result->num_rows > 0) {
 } else {
   echo "Keine Resultate vorhanden";
 }
-mysqli_close($conn);
+$row = mysqli_fetch_array( $northwind, MYSQLI_ASSOC)
 ?>
 <table>
 <tr>
 <td><?= $row['id']; ?></td>
 <td><?= $row['Vorname']; ?></td>
+<td><?= $row['Nachname']; ?></td>
+<td><?= $row['JobTitle']; ?></td>
 </tr>
 </table>
+<? echo "</table>";?>
